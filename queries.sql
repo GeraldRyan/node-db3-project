f2e43677ec0b346006656a0df3b3205469be848c
GeraldRyan <gerald.ryan40@yahoo.com>
2020-05-13 16:53:57 -0500

    progressssss

--- queries.sql
+++ queries.sql
@@ -1,8 +1,9 @@
 -- Multi-Table Query Practice
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
-
+select c.id, productname, categoryname from product as p inner join category as c on p.id = c.id
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+select o.id, s.companyname from "order" as o inner join shipper as s on o.shipvia = s.id
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -2,7 +2,8 @@ const db = require('../db-config')
 
 module.exports = {
   find,
-  findById
+  findById,
+  findSteps
 }
 
 function find() {
@@ -12,3 +13,27 @@ function find() {
 function findById(id) {
   return db('schemes').where({id}).first()
 }
+
+function findSteps(id){
+  return db('schemes').join('steps', "schemes.id",'=','steps_id').where('schemes.id', '=', id)
+
+}
+
+
+// -   `findSteps(id)`:
+//     -   Expects a scheme `id`.
+//     -   Resolves to an array of all correctly ordered step for the given scheme: `[ { id: 17, scheme_name: 'Find the Holy Grail', step_number: 1, instructions: 'quest'}, { id: 18, scheme_name: 'Find the Holy Grail', step_number: 2, instructions: '...and quest'}, etc. ]`.
+//     -   This array should include the `scheme_name` _not_ the `scheme_id`.
+// -   `add(scheme)`:
+//     -   Expects a scheme object.
+//     -   Inserts scheme into the database.
+//     -   Resolves to the newly inserted scheme, including `id`.
+// -   `update(changes, id)`:
+//     -   Expects a changes object and an `id`.
+//     -   Updates the scheme with the given id.
+//     -   Resolves to the newly updated scheme object.
+// -   `remove(id)`:
+//     -   Removes the scheme object with the provided id.
+//     -   Resolves to the removed scheme
+//     -   Resolves to `null` on an invalid id.
+//     -   (Hint: Only worry about removing the `scheme`. The database is configured to automatically remove all associated steps.)
